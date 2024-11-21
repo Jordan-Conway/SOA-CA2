7fe03f0d4410ff47d1447ee7e67e15cbb2b2a6e2
jordan <D00244954@student.dkit.ie>
2024-11-21 12:40:09 +0000

    Add DTOs for requests to add and delete images

---
+++ API/DTOs/Requests/AddImageRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs.Requests
+{
+    public class AddImageRequestDTO
+    {
+        public required System.Guid UserID { get; set; }
+        public required string Name { get; set; }
+        public required string Url { get; set; }
+    }
+}
---
+++ API/DTOs/Requests/DeleteImageRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.Requests
+{
+    public class DeleteImageRequestDTO
+    {
+        public required System.Guid UserID { get; set; }
+        public required System.Guid ImageID { get; set; }
+    }
+}
