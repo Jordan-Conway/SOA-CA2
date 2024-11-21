ebddb760d85ddcf47068e72162080568b1588896
jordan <D00244954@student.dkit.ie>
2024-11-21 12:34:33 +0000

    Add Responses Folder

--- API/DTOs/Responses/QuestionResponseDTO.cs
+++ API/DTOs/Responses/QuestionResponseDTO.cs
@@ -1,7 +1,7 @@
-﻿namespace API.DTOs.Requests
+﻿namespace API.DTOs.Responses
 {
     //This DTO represents a complete request from the client
-    public class QuestionRequestDTO
+    public class QuestionResponseDTO
     {
         public required ImageDto ImageOne { get; set; }
         public required ImageDto ImageTwo { get; set; }
