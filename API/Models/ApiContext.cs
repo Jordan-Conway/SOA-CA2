75c7c3992186d215bde3e2e2af0682f1eab2504e
jordan <D00244954@student.dkit.ie>
2024-11-21 11:36:28 +0000

    Add API Context

---
+++ API/Models/ApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+        public DbSet<ImageItem> Images { get; set; } = null!;
+        public DbSet<QuestionItem> QuestionItems { get; set; } = null !;
+    }
+}
