49c41af3992bb077a8007af4bed34f17b7437b5e
jordan <D00244954@student.dkit.ie>
2024-11-21 12:34:53 +0000

    Add DeleteQuestionRequestDTO

---
+++ API/DTOs/Requests/DeleteQuestionRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.Requests
+{
+    public class DeleteQuestionRequestDTO
+    {
+        public required System.Guid UserID { get; set; }
+        public required System.Guid QuestionId { get; set; }
+    }
+}
