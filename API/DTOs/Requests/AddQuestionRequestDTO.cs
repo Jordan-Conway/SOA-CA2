5abff23107f333bbb4ef5ec2909698815ecf9f2e
jordan <D00244954@student.dkit.ie>
2024-11-21 12:37:27 +0000

    Add AddQuestionRequestDTO

---
+++ API/DTOs/Requests/AddQuestionRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs.Requests
+{
+    public class AddQuestionRequestDTO
+    {
+        public required System.Guid UserID { get; set; }
+        public string? Text { get; set; }
+    }
+}
