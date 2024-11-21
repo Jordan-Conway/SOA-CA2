f23fd54aca08823e8c75a8e94635630164c6dbfe
jordan <D00244954@student.dkit.ie>
2024-11-21 12:31:32 +0000

    Move Request DTOs to separate folder

--- API/DTOs/Requests/QuestionRequestDTO.cs
+++ API/DTOs/Requests/QuestionRequestDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace API.DTOs
+﻿namespace API.DTOs.Requests
 {
     //This DTO represents a complete request from the client
     public class QuestionRequestDTO
---
+++ API/DTOs/Requests/VoteRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using API.Enums;
+
+namespace API.DTOs.Requests
+{
+    public class VoteRequestDto
+    {
+        public Guid ImageOneId { get; set; }
+        public Guid ImageTwoId { get; set; }
+
+        public Guid QuestionId { get; set; }
+
+        public Answer UserAnswer { get; set; }
+    }
+}
