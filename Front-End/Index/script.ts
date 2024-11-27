572cf6bb501b8f02340035a1c3061306bce4e148
Jordan <D00244954@student.dkit.ie>
2024-11-27 13:43:10 +0000

    Add front-end code skeleton

--- Front-End/Index/script.ts
+++ Front-End/Index/script.ts
@@ -1,3 +1,25 @@
-function test(): void {
-    console.log("Hello World")
+async function vote(choiceId: string, questionId: string): Promise<Boolean> {
+    throw new NotImplementedError("vote() is not implemented")
+    return true
+}
+
+async function getQuestion(): Promise<Data> {
+    throw new NotImplementedError("getQuestion() is not implemented")
+}
+
+interface Picture {
+    id: string,
+    name: string,
+    url: string,
+}
+
+interface Question {
+    id: string,
+    text: string
+}
+
+interface Data{
+    picture1: Picture,
+    picture2: Picture,
+    question: Question
 }
