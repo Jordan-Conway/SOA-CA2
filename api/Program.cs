bae630f9e95b8c61b7eba8c9b8f422fe5899b1e7
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2024-11-28 12:45:21 +0000

    Added swagger api view

--- api/Program.cs
+++ api/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<VoteEntryContext>(opt => opt.UseSqlite("Votes"));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,5 +27,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blazor API V1");
+});
 
 app.Run();
