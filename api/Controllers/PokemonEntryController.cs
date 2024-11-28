2b7e013e7ff8258ae2a1ab7b8f3c76927ccde0d7
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2024-11-28 12:15:25 +0000

    Created Controllers

---
+++ api/Controllers/PokemonEntryController.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonEntryController : ControllerBase
+    {
+        private readonly PokemonEntryContext _context;
+
+        public PokemonEntryController(PokemonEntryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PokemonEntry
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PokemonEntry>>> GetPokemonEntries()
+        {
+            return await _context.PokemonEntries.ToListAsync();
+        }
+
+        // GET: api/PokemonEntry/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PokemonEntry>> GetPokemonEntry(int id)
+        {
+            var pokemonEntry = await _context.PokemonEntries.FindAsync(id);
+
+            if (pokemonEntry == null)
+            {
+                return NotFound();
+            }
+
+            return pokemonEntry;
+        }
+
+        // PUT: api/PokemonEntry/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPokemonEntry(int id, PokemonEntry pokemonEntry)
+        {
+            if (id != pokemonEntry.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pokemonEntry).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PokemonEntryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PokemonEntry
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PokemonEntry>> PostPokemonEntry(PokemonEntry pokemonEntry)
+        {
+            _context.PokemonEntries.Add(pokemonEntry);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPokemonEntry", new { id = pokemonEntry.Id }, pokemonEntry);
+        }
+
+        // DELETE: api/PokemonEntry/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePokemonEntry(int id)
+        {
+            var pokemonEntry = await _context.PokemonEntries.FindAsync(id);
+            if (pokemonEntry == null)
+            {
+                return NotFound();
+            }
+
+            _context.PokemonEntries.Remove(pokemonEntry);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PokemonEntryExists(int id)
+        {
+            return _context.PokemonEntries.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ api/Controllers/QuestionEntryController.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionEntryController : ControllerBase
+    {
+        private readonly QuestionEntryContext _context;
+
+        public QuestionEntryController(QuestionEntryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/QuestionEntry
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<QuestionEntry>>> GetPokemonEntries()
+        {
+            return await _context.PokemonEntries.ToListAsync();
+        }
+
+        // GET: api/QuestionEntry/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<QuestionEntry>> GetQuestionEntry(int id)
+        {
+            var questionEntry = await _context.PokemonEntries.FindAsync(id);
+
+            if (questionEntry == null)
+            {
+                return NotFound();
+            }
+
+            return questionEntry;
+        }
+
+        // PUT: api/QuestionEntry/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuestionEntry(int id, QuestionEntry questionEntry)
+        {
+            if (id != questionEntry.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(questionEntry).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionEntryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/QuestionEntry
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<QuestionEntry>> PostQuestionEntry(QuestionEntry questionEntry)
+        {
+            _context.PokemonEntries.Add(questionEntry);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuestionEntry", new { id = questionEntry.Id }, questionEntry);
+        }
+
+        // DELETE: api/QuestionEntry/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuestionEntry(int id)
+        {
+            var questionEntry = await _context.PokemonEntries.FindAsync(id);
+            if (questionEntry == null)
+            {
+                return NotFound();
+            }
+
+            _context.PokemonEntries.Remove(questionEntry);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool QuestionEntryExists(int id)
+        {
+            return _context.PokemonEntries.Any(e => e.Id == id);
+        }
+    }
+}
