929c84beb7d4c84ad33ab656261f87f8a818f3cf
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2024-12-05 12:00:03 +0000

    Cast vote API

--- api/Controllers/VoteController.cs
+++ api/Controllers/VoteController.cs
@@ -20,6 +20,70 @@ namespace api.Controllers
             _context = context;
         }
 
+        [HttpPost("cast")]
+        public async Task<ActionResult<VoteEntry>> CastVote(VoteDTO vote)
+        {
+            var voteEntry = await _context.VoteEntry.FindAsync(vote.PokemonId, vote.QuestionId);
+
+            if (voteEntry == null)
+            {
+                var pokemonEntry = await _context.PokemonEntry.FindAsync(vote.PokemonId);
+                var questionEntry = await _context.QuestionEntry.FindAsync(vote.QuestionId);
+
+                if (pokemonEntry == null || questionEntry == null)
+                {
+                    return NotFound();
+                }
+
+                voteEntry = new VoteEntry();
+                voteEntry.PokemonId = vote.PokemonId;
+                voteEntry.QuestionId = vote.QuestionId;
+                voteEntry.VoteCount = 1;
+
+                _context.VoteEntry.Add(voteEntry);
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException)
+                {
+                    if (VoteEntryExists(voteEntry.PokemonId))
+                    {
+                        return Conflict();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return CreatedAtAction("GetVoteEntry", new { id = voteEntry.PokemonId }, voteEntry);
+            }
+
+            voteEntry.VoteCount += 1;
+            _context.Entry(voteEntry).State = EntityState.Modified;
+
+            _context.VoteEntry.Add(voteEntry);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (VoteEntryExists(voteEntry.PokemonId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
         // GET: api/Vote
         [HttpGet]
         public async Task<ActionResult<IEnumerable<VoteEntry>>> GetVoteEntry()
