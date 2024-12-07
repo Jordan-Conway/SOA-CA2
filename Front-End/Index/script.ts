5e4d5fb59ca86f427b23e3cd96d03bb3cd6ca3cc
Jordan <D00244954@student.dkit.ie>
2024-12-07 15:57:49 +0000

    Change pokemon to be gotten randomly

--- Front-End/Index/script.ts
+++ Front-End/Index/script.ts
@@ -1,11 +1,13 @@
+let apiUrl = "https://localhost:7015/api/"
+
 let loginButton: Element
 let loginModal: HTMLDialogElement
 let createQuestionForm: HTMLFormElement
 
-let image1: Element
-let image2: Element
-let image1Url: string = "../Images/1.jpeg"
-let image2Url: string = "../Images/2.jpeg"
+let imageElement1: Element
+let imageElement2: Element
+let image1: Picture
+let image2: Picture
 
 document.addEventListener('DOMContentLoaded', function() {
     load()
@@ -18,8 +20,8 @@ function load()
     loginModal = document.getElementById("loginDialog") as HTMLDialogElement
     createQuestionForm = document.getElementById("questionForm") as HTMLFormElement
 
-    image1 = document.getElementById("picture1")
-    image2 = document.getElementById("picture2")
+    imageElement1 = document.getElementById("picture1")
+    imageElement2 = document.getElementById("picture2")
 
     loginModal.close()
 
@@ -34,10 +36,15 @@ function load()
     })
 }
 
-function loadImages()
+async function loadImages()
 {
-    image1.setAttribute("src", image1Url)
-    image2.setAttribute("src", image2Url)
+    let pokemon1 = await getPokemon()
+    let pokemon2 = await getPokemon()
+    image1 = pokemon1
+    image2 = pokemon2
+
+    imageElement1.setAttribute("src", image1.imageUrl)
+    imageElement2.setAttribute("src", image2.imageUrl)
 }
 
 async function vote(choiceId: string, questionId: string): Promise<Boolean> {
@@ -45,7 +52,16 @@ async function vote(choiceId: string, questionId: string): Promise<Boolean> {
     return true
 }
 
-async function getQuestion(): Promise<Data> {
+//https://stackoverflow.com/a/60166572
+async function getPokemon(): Promise<Picture> {
+    let url = apiUrl + "Pokemon/random"
+    
+    return fetch(url)
+        .then((res) => res.json())
+        .then((res) => {return res as Promise<Picture>})
+}
+
+async function getQuestion(): Promise<Question> {
     throw new NotImplementedError("getQuestion() is not implemented")
 }
 
@@ -111,7 +127,7 @@ function toggleRegister(): void {
 interface Picture {
     id: string,
     name: string,
-    url: string,
+    imageUrl: string,
 }
 
 interface Question {
