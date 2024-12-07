7c88da1373c0c60651d3ceb24f8d4e02be5fb16d
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2024-12-07 12:52:47 +0000

    Refactored to used Interface infront of DB

--- api/Controllers/PokemonController.cs
+++ api/Controllers/PokemonController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using api.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
+using api.DrivenAdapters.DatabaseApaters;
 
 namespace api.Controllers
 {
@@ -14,70 +15,34 @@ namespace api.Controllers
     [ApiController]
     public class PokemonController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private readonly SqliteAdapter sqliteAdapter;
 
         public PokemonController(AppDbContext context)
         {
-            _context = context;
-        }
-
-        // GET: api/Pokemon
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<PokemonDTO>>> GetPokemonEntry()
-        {
-            var pokemonResultList = await _context.PokemonEntry.ToListAsync();
-            var pokemonList = new List<PokemonDTO>();
-            pokemonResultList.ForEach(pokemon =>
-            {
-                pokemonList.Add(new PokemonDTO(pokemon.Id, pokemon.Name, pokemon.ImageUrl));
-            });
-            return pokemonList;
-        }
-
-        // GET: api/Pokemon/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<PokemonDTO>> GetPokemonEntry(int id)
-        {
-            var pokemonEntry = await _context.PokemonEntry.FindAsync(id);
-
-            if (pokemonEntry == null)
-            {
-                return NotFound();
-            }
-
-            return new PokemonDTO(pokemonEntry.Id, pokemonEntry.Name, pokemonEntry.ImageUrl);
+            sqliteAdapter = new SqliteAdapter(context);
         }
 
         [HttpGet("random")]
         public async Task<ActionResult<PokemonDTO>> GetRandomPokemonEntry()
         {
-            const int POKEMON_MIN_ID = 1;
-            const int POKEMON_MAX_ID = 1024;
-            var random = new Random();
-            var pokemonEntry = await _context.PokemonEntry.FindAsync(random.Next(POKEMON_MIN_ID, POKEMON_MAX_ID + 1));
+            var pokemon = await sqliteAdapter.getRandomPokemon();
 
-            if (pokemonEntry == null)
+            if (pokemon == null)
             {
                 return StatusCode(500);
             }
 
-            return new PokemonDTO(pokemonEntry.Id, pokemonEntry.Name, pokemonEntry.ImageUrl);
+            return pokemon;
         }
 
+        // Used to construct the database.
         // POST: api/Pokemon
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<PokemonEntry>> PostPokemonEntry(PokemonEntry pokemonEntry)
+        public async Task<ActionResult<PokemonDTO>> PostPokemonEntry(PokemonDTO pokemon)
         {
-            _context.PokemonEntry.Add(pokemonEntry);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPokemonEntry", new { id = pokemonEntry.Id }, pokemonEntry);
-        }
-
-        private bool PokemonEntryExists(int id)
-        {
-            return _context.PokemonEntry.Any(e => e.Id == id);
+            var pokemonResult = sqliteAdapter.CreatePokemon(pokemon);
+            return CreatedAtAction("GetPokemonEntry", new { id = pokemonResult.Id }, pokemonResult);
         }
     }
 }
