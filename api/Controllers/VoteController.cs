3564fb55b2fae8a23516480d898508ca53a22183
Mayv-dev <91346624+Mayv-dev@users.noreply.github.com>
2024-12-07 20:48:34 +0000

    Changed displayed URL

--- api/Controllers/VoteController.cs
+++ api/Controllers/VoteController.cs
@@ -28,7 +28,7 @@ namespace api.Controllers
             await sqliteAdapter.CastVote(vote.PokemonId, vote.QuestionId);
         }
 
-        [HttpGet("results/{id}")]
+        [HttpGet("results/{questionId}")]
         public async Task<ActionResult<ResultDTO>> GetResults(int questionId)
         {
             var results = await sqliteAdapter.GetResults(questionId);
